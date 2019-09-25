86045582a9024aada09e955aa784bcf3f70c9a8d
artinsepanta <faribasalehi@hotmail.com>
2019-09-24 20:48:35 -0500

    2-3 steps

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 4001;
+const usersRouter = require('./routes/employees');
+app.get('/', (req, res) => res.send('Welcome to our API'))
+
+//Body Paser Middlewear
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//users Api routes
+app.use('/api',usersRouter);
+
+app.listen(port, () => {
+  console.log('app is listening on:', port)
+})
