26be57f6297e27a1e87fce032b3822a317ef2614
grantkee <grant.d.kee@gmail.com>
2019-09-24 20:45:20 -0500

    GET route for all employees

--- routes/employees.js
+++ routes/employees.js
@@ -1,5 +1,7 @@
 // router.get('/', usersController.getAllUsers)
-router.get("/");
+router.get("/", (res,req)=>{
+  res.send('getting employees')
+});
 router.get("/:id");
 
 router.get("/firstname/:first_name", (req, res) => {
