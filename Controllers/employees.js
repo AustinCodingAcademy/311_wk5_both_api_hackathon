daabf38982e4b2f6eeb3d342b03b2eb852238967
jonathan gregis <jongregis@jonathans-MacBook-Pro-2.local>
2019-09-24 20:49:39 -0500

    added controllers and mysql

---
+++ Controllers/employees.js
@@ -0,0 +1,39 @@
+const mysql = require("mysql");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
+
+const getEmployees = () => {
+  pool.query("SELECT * FROM employees", (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const getEmployeesById = () => {
+  let sql = "SELECT ?? FROM ?? WHERE ?? = ?";
+  const replacements = ["*", "employees", "id", req.params.id];
+
+  sql = mysql.format(sql, replacements);
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const getEmployeesByFirstName = () => {
+  let sql = "SELECT FROM ?? WHERE ?? = ?";
+  const replacements = ["employees", "first_name", req.params.first_name];
+
+  sql = mysql.format(sql, replacements);
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+  });
+};
+
+module.exports = {
+  getEmployees,
+  getEmployeesById,
+  getEmployeesByFirstName
+};
