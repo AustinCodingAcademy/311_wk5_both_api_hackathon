e1f770ba56b24689c401529ab956d7bbe3fd4163
fagner henrique flores <henriquefagner@hotmail.com>
2019-09-26 19:36:20 -0500

    done rk get employees

--- routes/employees.js
+++ routes/employees.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 const usersController = require("../controllers/employees");
 router.get("/");
-=======
+
 // router.get('/', usersController.getAllUsers)
-router.get("/", (res,req)=>{
+router.get("/", (req,res)=>{
   res.send('getting employees')
 });
->>>>>>> 26be57f6297e27a1e87fce032b3822a317ef2614
+
 router.get("/:id");
 
 router.get("/firstname/:first_name", (req, res) => {
