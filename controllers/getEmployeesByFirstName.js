dbfeb8d746ada9f6703ecc8cb8c6e9214509f407
grantkee <grant.d.kee@gmail.com>
2019-09-28 11:31:21 -0500

    it works!!

--- controllers/getEmployeesByFirstName.js
+++ controllers/getEmployeesByFirstName.js
@@ -4,8 +4,8 @@ const { handleSQLError } = require("../mysql/error");
 
 const getEmployeesByFirstName = (req, res) => {
 	let sql = "SELECT * FROM ?? WHERE ?? = ?";
-	sql = mysql.format(sql, ["users", "first_name", req.params.first_name]);
-	res.json(sql);
+	sql = mysql.format(sql, ["employees", "first_name", req.params.first_name]);
+
 
 	pool.query(sql, (err, rows) => {
 		if (err) return handleSQLError(res, err);
