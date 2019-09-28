0d923ed79fbdd57ac7db5368b1af6803992f994d
jaywiles <jay.wiles@gmail.com>
2019-09-28 11:01:29 -0500

    new routes added

---
+++ controllers/allDepartments.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+// const employeesControllers = require('./employees')
+const pool = require('../mysql/connection')
+
+const getIdWithSalary = (req, res) => {
+    // let sql = "SELECT * FROM ?? LIMIT ?";
+    // let replacements = [ 'employees', 50 ];
+    // sql = mysql.format(sql, replacements);
+
+    // pool.query(sql, (err, rows) => {
+    //     if (err) return res.status(500).send('something went wrong');
+    //     return res.json(rows);
+    // })
+}
+
+module.exports = { getIdWithSalary }
--- routes/employees.js
+++ routes/employees.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const empCont = require('../controllers/getEmployees');
-const empIdCont = require('../controllers/getEmployeesById');
-const empNameCont = require('../controllers/getEmployeesByFirstName');
+const employeeController = require('../controllers/getEmployees');
+const employeeByIdController = require('../controllers/getEmployeesById');
+const employeeByNameController = require('../controllers/getEmployeesByFirstName');
+const employeeByIdWithSalary = require('../controllers/employeeByIdWithSalary');
+const employeeByIdWithDepartment = require('../controllers/employeeByIdWithDepartment');
+const salaryByEmployeeId = require('../controllers/salaryByEmployeeId');
+const allDepartments = require('../controllers/allDepartments');
+const allTitles = require('../controllers/allTitles');
 
-router.get('/employees', empCont.getEmployees);
-router.get('/employees/:emp_no', empIdCont.getEmployeesById);
-router.get('/employees/firstname/:first_name', empNameCont.getEmployeesByFirstName);
+router.get('/employees', employeeController.getEmployees);
+router.get('/employees/:emp_no', employeeByIdController.getEmployeesById);
+router.get('/employees/firstname/:first_name', employeeByNameController.getEmployeesByFirstName);
+router.get('/salaries/employees/:id', employeeByIdWithSalary.getIdWithSalary);
+router.get('/departments/employees/:id', employeeByIdWithDepartment.getIdWithDepartment);
+router.get('/salaries/:empid', salaryByEmployeeId.getSalaryById);
+router.get('/departments', allDepartments.getAllDepartments);
+router.get('/titles', allTitles.getAllTitles);
 
 module.exports = router;
