1b7324c3783731066cf7aa746d6c174775e8dff6
Anna Goncharova <anna.gonch.liaison@gmail.com>
2019-09-28 09:19:56 -0500

    092819_AG

--- controllers/getEmployeesById.js
+++ controllers/getEmployeesById.js
@@ -1,13 +1,13 @@
 const getEmployeesById = (req, res) => {
-  // SELCT USERS WHERE IS = <REQ PARAMS ID>;
-  let sql = "SELECT * FROM ?? WHERE ?? = ?";
-  //WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, ["employees", "emp_no", req.params.id]);
+	// SELCT USERS WHERE IS = <REQ PARAMS ID>;
+	let sql = "SELECT * FROM ?? WHERE ?? = ?";
+	//WHAT GOES IN THE BRACKETS
+	sql = mysql.format(sql, ["employees", "emp_no", req.params.id]);
 
-  pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err);
-    return res.json(rows);
-  });
+	pool.query(sql, (err, rows) => {
+		if (err) return handleSQLError(res, err);
+		return res.json(rows);
+	});
 };
 
 module.exports = { getEmployeesById };
