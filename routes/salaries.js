025cc744366f51b7e1df23b273e58bcbda33c1dd
grantkee <grant.d.kee@gmail.com>
2019-10-01 19:16:44 -0500

    found an error in the router where we were calling the wrong file in the controller, however now the server never sends a response

--- routes/salaries.js
+++ routes/salaries.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const getEmployeesSalaries = require("../controllers/getEmployeesSalaries");
 const getEmployeesSalariesById = require("../controllers/getEmployeesSalariesById");
-const employeesSalariesFirstName = require("../controllers/getEmployeesByFirstName");
+const employeesSalariesFirstName = require("../controllers/getEmployeesSalariesFirstname");
 
 router.get("/salaries", getEmployeesSalaries.getEmployeesSalaries);
 
 router.get("/salaries/:id", getEmployeesSalariesById.getEmployeesSalariesById);
 
-//router.get("/salaries/:firstname", employeesSalariesFirstName.getEmployeesSalariesById);
+router.get("/salaries/firstname/:firstname", employeesSalariesFirstName.getEmployeesSalariesFirstName);
 
 module.exports = router;
