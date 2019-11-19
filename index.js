ff7969201d67288f5f138f5531d5815d38563065
Your Name <you@example.com>
2019-11-18 20:31:26 -0600

    server up and running

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("retesting")
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4001
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/' , (res, send) =>{
+    res.json("Server is running")
+})
+
+app.listen(port, () =>{
+  console.log(`Example app listening on port ${port}!`)
+})
