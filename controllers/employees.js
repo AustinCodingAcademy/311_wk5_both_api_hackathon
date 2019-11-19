5b573919b9be846a31bc7afffcd2ff3ec29d4dee
David Bille <d_bille@yahoo.com>
2019-11-19 07:09:49 -0600

    cleaned up code in getEmployees

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,28 +1,24 @@
-const mysql = require('mysql')
-const pool = require ('../mysql/connection')
-const sqlErrorHandler = require('../mysql/error')
+const mysql = require("mysql");
+const pool = require("../mysql/connection");
+const sqlErrorHandler = require("../mysql/error");
 
 const getEmployees = (req, res) => {
-
-  let sql = 'SELECT * FROM ?? LIMIT ?'
-  let replacements = ['employees.employees', 50]
-  sql = mysql.format(sql, replacements)
+  let sql = "SELECT * FROM ?? LIMIT ?";
+  let replacements = ["employees.employees", 50];
+  sql = mysql.format(sql, replacements);
 
   pool.query(sql, (err, results) => {
-    if (err) {
-      return sqlErrorHandler(res, err)
-    } else {
-      return res.json(results)
-    }
-  })
+    if (err) return sqlErrorHandler(res, err);
+    return res.json(results);
+  });
 };
 
 const getEmployeesById = (req, res) => {
-  res.send('getting employees...')
-}
+  res.send("getting employees...");
+};
 
 const getEmployeesByFirstName = (req, res) => {
-  res.send('getting employees...')
-}
+  res.send("getting employees...");
+};
 
-module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
+module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName };
