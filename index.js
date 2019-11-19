4767b2741e19bb72d1ce327f20ff2778e797d604
amberstyers <amberstyers@gmail.com>
2019-11-18 20:28:24 -0600

    basic express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('re-testing');
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 4001
+app.get('/', (req, res) => res.send('Welcome to our API '))
+app.listen(port, () => {
+  console.log('app is listening on:', port)
+})
+
