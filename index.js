5899e5d9890f417fefc3864a69cf874ffa8ca4eb
galvanitic <develop@rjgalvan.com>
2019-11-18 19:49:23 -0600

    Added basic express functionality.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('test');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4001;
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Rodolfo & Eugene's API");
+})
+
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}`);
+})
+
