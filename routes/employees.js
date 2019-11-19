c19edda0ab9004080990ce794d405fd090d3cea2
David Bille <d_bille@yahoo.com>
2019-11-19 06:41:27 -0600

    linked routes to controllers.

--- routes/employees.js
+++ routes/employees.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const employeesController = require('../controllers/employees')
 
-router.get('/', (res, req) => {
-  
-});
+router.get('/', employeesController.getEmployees);
 
 router.get('/:id', (res, req) => {
   res.send('getting employees...')
 });
 
-router.get('firstname/:first_name', (res, req) => {
+router.get('/firstname/:first_name', (res, req) => {
   res.send('getting employees...')
 });
 
