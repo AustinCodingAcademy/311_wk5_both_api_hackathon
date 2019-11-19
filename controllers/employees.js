248e6c50a128b6e5b8f92dd47dc8b516991a7d18
galvanitic <develop@rjgalvan.com>
2019-11-18 20:57:38 -0600

    Added dummy controllers.

---
+++ controllers/employees.js
@@ -0,0 +1,18 @@
+// Create the following functions 
+// getEmployees, getEmployeesById, getEmployeesByFirstName
+// const employees = require('');
+
+//getEmployees
+exports.getEmployees = function getEmployees(req, res) {
+  res.send("getting employees...");
+}
+
+//getEmployeesById
+exports.getEmployeesById = function getEmployeesById(req, res) {
+  res.send("getting employees...");
+}
+
+//getEmployeesByFirstName
+exports.getEmployeesByFirstName = function getEmployeesByFirstName(req, res) {
+  res.send("getting employees...");
+}
