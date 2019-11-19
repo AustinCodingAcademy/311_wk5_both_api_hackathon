6e52b8b53ee2cb39054622ab8db33f2a3ab99b78
jlmeister <iamphil4483@yahoo.com>
2019-11-18 20:37:48 -0600

    finished router

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express()
+const app = express();
+const employeesRouter = require('./routes/employees');
 const port = process.env.PORT || 4001
+app.use('/employees', employeesRouter);
 app.get('/', (req, res) => res.send('Welcome to our API '))
 app.listen(port, () => {
   console.log('app is listening on:', port)
