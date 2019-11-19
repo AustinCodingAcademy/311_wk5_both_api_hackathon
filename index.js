4d076a7037b0f0b396278c08b2006aae3f1d06a4
kevinjj30 <kevinjenkins56@yahoo.com>
2019-11-18 19:35:29 -0600

    second push

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('retesting')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4001;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our API!')
+  })
+
+app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}!`);
+   });
+
