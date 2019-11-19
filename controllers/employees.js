accc7f9d61d600752ac5c997dc707049906940b0
Eugene Ignatov <ignatovyevgen@gmail.com>
2019-11-18 22:07:09 -0600

    connecting databases

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,18 +1,27 @@
-// Create the following functions 
-// getEmployees, getEmployeesById, getEmployeesByFirstName
-// const employees = require('');
+const mysql = require('mysql');
+const pool = require('../mysql/connection');
+const { handleSQLError } = require('../mysql/error');
 
 //getEmployees
 exports.getEmployees = function getEmployees(req, res) {
-  res.send("getting employees...");
-}
+	// pool.query('SELECT * FROM employees', (err, rows) => {
+	// 	if (err) return handleSQLError(res, err);
+	// 	return res.json(rows);
+	// });
+	res.send('hey hey');
+};
 
 //getEmployeesById
 exports.getEmployeesById = function getEmployeesById(req, res) {
-  res.send("getting employees...");
-}
+	res.send('getting employees...');
+};
 
 //getEmployeesByFirstName
 exports.getEmployeesByFirstName = function getEmployeesByFirstName(req, res) {
-  res.send("getting employees...");
-}
+	pool.query('SELECT * FROM users where first_name = ?', (err, rows) => {
+		sql = mysql.format(sql, req.body.first_name);
+
+		if (err) return handleSQLError(res, err);
+		return res.json(rows);
+	});
+};
