3603f7fa502d207dbd980df94346adae00c7cb13
Your Name <you@example.com>
2019-11-20 13:00:16 -0600

    router file established

---
+++ routes/employees.js
@@ -0,0 +1,14 @@
+
+
+
+const express = require('express');
+const router = express.Router();
+const employeesController = require('../controllers/employees')
+
+router.get('/employees', employeesController.list);
+router.get('/employees:id', employeesController.show);
+router.get('/employees:firstName', employeesController.first);
+
+module.exports = router 
+
+
