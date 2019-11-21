86e60f750b095cde219aa6af82973e2bb56a75ba
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-11-20 20:14:31 -0600

    added to getEmployees

--- contollers/getEmployees.js
+++ contollers/getEmployees.js
@@ -1,8 +1,28 @@
-const employees = require('');
+const mysql = require('../node_modules/mysql')
+// const employees = require('../node_modules/mysql/');
 
-function getEmployees(request,response) {
-  return response.json(employees);
-}
+
+var con = mysql.createConnection({
+  host: "35.223.66.40",
+  user: "root",
+  password: "coding",
+  database: "admin"
+});
+
+
+// getEmployees
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("select * from employees where emp_no < 10051", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+// function getEmployees(request,response) {
+//   // let allEmployees = 
+//   return response.json(employees);
+// }
 
 
 module.exports = {getEmployees};
