53c0fca7762ff0a7692ac4192b748d89b8210d2b
Alex Munoz <anmunoz24@gmail.com>
2019-11-20 19:00:24 -0800

    departments

--- routes/employees.js
+++ routes/employees.js
@@ -10,4 +10,5 @@ router.get('/firstname/:first_name', employeesController.getEmployeesByFirstName
 
 router.get('/salaries/employees/:emp_no', employeesController.getEmployeesSalaries);
 
+router.get('/departments/:dept_name', employeesController.getEmployeesDepartment);
   module.exports = router 
