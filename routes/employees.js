59a652deac4cf5d95264daaa33725e71e9343426
Eugene Ignatov <ignatovyevgen@gmail.com>
2019-11-20 19:27:42 -0600

    text

--- routes/employees.js
+++ routes/employees.js
@@ -13,9 +13,12 @@ router.get('/employees/firstname/:first_name', getEmployeesByFirstName);
 
 module.exports = router;
 
-<<<<<<< HEAD
 console.log('f');
-=======
-
-I'm sick af
->>>>>>> aade9b8103b1afffaef94920f64905749d13af6a
+console.log('f');
+console.log('f');
+console.log('f');
+console.log('f');
+console.log('f');
+console.log('f');
+console.log('f');
+console.log('f');
