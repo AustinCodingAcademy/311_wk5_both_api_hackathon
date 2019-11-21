a690c7d75a0d8e86efea236d5fafbfc4fc347d22
jlmeister <iamphil4483@yahoo.com>
2019-11-20 19:28:00 -0600

    edited employees router to include controller file, and created mysql connection instance.

--- routes/employees.js
+++ routes/employees.js
@@ -3,13 +3,7 @@ const router = express.Router();
 
 const employeesController = require('../controllers/employees')
 
-router.get('/', (req, res) => {
-  res.send('getting employees...');
-})
-router.get('/:id', (req, res) => {
-  res.send('getting employees...');
-})
-router.get('/firstname/:first_name', (req, res) => {
-  res.send('getting employees...');
-})
+router.get('/', employeesController.getEmployees)
+router.get('/:id', employeesController.getEmployeesById)
+router.get('/firstname/:first_name', employeesController.getEmployeesByFirstName)
 module.exports = router;
