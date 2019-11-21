709502be8e73294fa408463acf141a700cbfbfbd
amberstyers <amberstyers@gmail.com>
2019-11-20 21:39:07 -0600

    update on get employees by id

--- controllers/getEmployeesById.js
+++ controllers/getEmployeesById.js
@@ -1,4 +1,19 @@
+const mysql = require('mysql')
+const db = require('../mysql/connection')
+
 const getEmployeesById = (req, res) => {
-  res.send('getting employees...');
+  // select all fields from emp table where emp # matches id query param
+  let sql = 'SELECT * FROM ?? WHERE ?? = ?'
+  const replacements = ['employees', 'emp_no', req.params.id]
+  sql = mysql.format(sql, replacements)
+  db.pool.query(sql, (err, results) => {
+    if (err) throw err;
+    return res.status(204).json(results);
+  
+  })
 }
+
+
+
 module.exports = getEmployeesById;
+
