49222a4f94ba070a36b73c87bcebc3d3c6161a3f
Your Name <you@example.com>
2019-11-20 20:31:18 -0600

    latest

--- routes/employees.js
+++ routes/employees.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const employeesController = require('../controllers/employees')
 
 router.get('/employees', employeesController.getEmployees);
-router.get('/employees:emp_no', employeesController.getEmployeesById);
-router.get('/employees:first_name', employeesController.getEmployeesByFirstName);
+router.get('/employees/:emp_no', employeesController.getEmployeesById);
+router.get('/employees/:first_name', employeesController.getEmployeesByFirstName);
 
 
 
