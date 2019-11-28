b8b86a04aa4b7e87804740b17e3fb5d4451f4c70
Your Name <you@example.com>
2019-11-27 21:36:29 -0600

    latest work for review

--- controllers/employees.js
+++ controllers/employees.js
@@ -35,8 +35,36 @@ const getEmployeesByFirstName = (req, res) => {
     });
 }
 
+//Return the employee by id with salary information
+//There should be a property on the employee object called salary with a number value
+const getEmployeesSalary = (req, res) => {
+    let sql = 'SELECT salaries FROM employees WHERE emp_no = ? limit 10';
+    let replace = [req.params.emp_no]
+    sql = mysql.format(sql, replace);
+    
+    pool.query(sql, (err, rows) => {
+        if(err) return handleSQLError(res, err);
+        return res.json(rows);
+    });
+}
+
+//Return the employee with department information
+//There should be a property on the employee object called departments that is an array
+const getEmployeesDepartment = (req,res) => {
+    let sql = 'SELECT * FROM employees WHERE ?? = ? limit 10'; 
+    let replace = [req.params.department]
+    sql = mysql.format(sql, replace);
+    
+    pool.query(sql, (err, rows) => {
+        if(err) return handleSQLError(res, err);
+        return res.json(rows);
+    });
+}
 module.exports = {
     getEmployees,
     getEmployeesById,
-    getEmployeesByFirstName
+    getEmployeesByFirstName,
+    getEmployeesSalary,
+    getEmployeesDepartment,
+    getOneSalaryById
 }
--- routes/employees.js
+++ routes/employees.js
@@ -8,6 +8,15 @@ const employeesController = require('../controllers/employees')
 router.get('/employees', employeesController.getEmployees);
 router.get('/employees/:emp_no', employeesController.getEmployeesById);
 router.get('/first_name/:first_name', employeesController.getEmployeesByFirstName);
+//Return the employee by id with salary information
+//There should be a property on the employee object called salary with a number value
+router.get('/salaries/employees/:id', employeesController.getEmployeesSalary);
+//Return the employee with department information
+//There should be a property on the employee object called departments that is an array
+router.get('/departments/employees/:id', employeesController.getEmployeesDepartment);
+//Return the salary of one employee by id
+//Does it make sense to show all salaries?
+router.get('/salaries/:empid', employeesController.getOneSalaryById);
 
 
 
