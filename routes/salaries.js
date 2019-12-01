6460077b864e668d11017794f4af347b04e8d4da
amberstyers <amberstyers@gmail.com>
2019-12-01 15:25:24 -0600

    salaries stuff

---
+++ routes/salaries.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const salariesController = require('../controllers/salaries');
+
+router.get('/highpay', salariesController.getHighestPaidEmployees);
+
+router.get('/sumsalary', salariesController.sumOfSalaries);
+
+router.get('/:id', salariesController.getSalariesById);
+
+module.exports = router; 
