57eb08cc9e1b8feea3d1f6d3b539ea8e1b0a8729
amberstyers <amberstyers@gmail.com>
2019-12-01 16:52:12 -0600

    fixing controllers

--- controllers/salaries.js
+++ controllers/salaries.js
@@ -3,26 +3,33 @@ const mysql = require("mysql");
 const db = require("../mysql/connection");
 const sqlErrorHandler = require("../mysql/error");
 
-const getHighestPaidEmployees = (req, res) => {
-  let sql = "SELECT ??, ??, ?? FROM ?? JOIN ?? ON ?? = ?? ORDER BY ?? DESC LIMIT ?";
-  let replacements = [
-    "first_name",
-    "last_name",
-    "salary",
-    "employees.employees",
-    "employees.salaries",
-    "employees.emp_no",
-    "employees.salaries.emp_no",
-    "salary",
-    10
-  ];
-  sql = mysql.format(sql, replacements);
+// const getHighestPaidEmployees = (req, res) => {
+//   let sql = "SELECT ??, ??, ?? FROM ?? JOIN ?? ON ?? = ?? ORDER BY ?? DESC LIMIT ?";
+//   let replacements = [
+//     "first_name",
+//     "last_name",
+//     "salary",
+//     "employees.employees",
+//     "employees.salaries",
+//     "employees.emp_no",
+//     "employees.salaries.emp_no",
+//     "salary",
+//     10
+//   ];
+//   sql = mysql.format(sql, replacements);
 
-  db.query(sql, (err, results) => {
-    if (err) return sqlErrorHandler(res, err);
-    return res.json(results);
-  });
-};
+//   db.query(sql, (err, results) => {
+//     if (err) return sqlErrorHandler(res, err);
+//     return res.json(results);
+//   });
+// };
+
+const getSalaries = (req, res) => {
+  db.query("SELECT * FROM salaries LIMIT 50", (err, rows) => {
+    if (err) return sqlErrorHandler(res, err)
+    return res.json(rows);
+  })
+}
 
 const getSalariesById = (req, res) => {
   let sql = "SELECT * FROM ?? WHERE ?? = ?";
@@ -44,4 +51,4 @@ const sumOfSalaries = (req, res) => {
   });
 };
 
-module.exports = { getHighestPaidEmployees, getSalariesById, sumOfSalaries };
+module.exports = { getHighestPaidEmployees, getSalariesById, sumOfSalaries, getSalaries };
