7f78120e247ab7824daae95cfdf6940aee504aa0
amberstyers <amberstyers@gmail.com>
2019-12-01 15:30:25 -0600

    more salaries stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const employeesRouter = require('./routes/employees');
+const salariesRouter = require('./routes/salaries');
 const port = process.env.PORT || 4001
+app.use('/salaries', salariesRouter);
 app.use('/employees', employeesRouter);
 app.get('/', (req, res) => res.send('Welcome to GOLD TABLErs'))
 app.listen(port, () => {
