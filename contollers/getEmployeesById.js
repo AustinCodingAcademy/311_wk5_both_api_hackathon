2425ff0202c50b3fd53f7fe9193e1eadc0b51f8e
emphillips <matt-phillips@live.com>
2019-12-10 19:31:42 -0600

    updated controllers

--- contollers/getEmployeesById.js
+++ contollers/getEmployeesById.js
@@ -1,8 +1,22 @@
-const employeeId = require('');
+var con = mysql.createConnection({
+  host: "35.223.66.40",
+  user: "root",
+  password: "coding",
+  database: "admin"
+});
 
-function getEmployeesById(request,response) {
-  return response.json(employeeId);
-}
+// getEmployeesByid
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("select * from employees where emp_no < 10051", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
+// function getEmployees(request,response) {
+//   // let allEmployees = 
+//   return response.json(employees);
+// }
 
 module.exports = {getEmployeesById};
