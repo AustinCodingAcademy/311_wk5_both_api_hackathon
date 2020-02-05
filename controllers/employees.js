6e4a77e635851a059363b33bcb3b297630516550
dgasch512 <gaschdav208@gmail.com>
2020-02-04 20:34:05 -0600

    Added error file and controllers logic

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,14 +1,33 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
 
-let pool = require('../mysql/connection')
-
-const getEmployees = (req,res) => {
-  res.send('getting employees...')
+const getEmployees = (req, res) => {
+  pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
 };
 const getEmployeesById = (req,res) => {
-  res.send('getting employees...')
+  let sql = 'SELECT * FROM ?? WHERE ?? = ?';
+  const replacements = ['employees', 'emp_no', req.params.emp_no]
+  sql = mysql.format(sql, replacements)
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
 };
-const getEmployeesByFirstName = (req,res) => {
-  res.send('getting employees...')
+
+const getEmployeesByFirstName = (req, res) => {
+  let sql = 'SELECT * FROM ?? WHERE ?? = ?';
+  const replacements = ['employees', 'first_name', req.params.first_name]
+  sql = mysql.format(sql, replacements)
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
 };
 
 
