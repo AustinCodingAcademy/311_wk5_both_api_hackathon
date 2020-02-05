b8e3887cbd54f738b1729229ad0bc32dc8faa42a
ajewe <ajewe@utexas.edu>
2020-02-04 20:43:56 -0600

    get employees by first name

--- controllers/getAllEmployees.js
+++ controllers/getAllEmployees.js
@@ -9,15 +9,4 @@ const getEmployees = (req, res) => {
   })
 }
 
-// exports.getEmployeesById = function show(req, res) {
-//   const found = employees.some(employees => employees.id === parseInt(req.params.id));
-
-//   if (found) {
-//     res.json(employees.find(employees => employees.id === parseInt(req.params.id))); 
-//   } else {
-//     res.status(404).json({ msg: `No employee with the id of ${req.params.id}`});
-//   }
-// }
-
-
 module.exports = {getEmployees}
