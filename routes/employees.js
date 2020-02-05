df8877eb3d72b325975895852bb9d9f64e737ab8
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-02-04 20:52:24 -0600

    oh yeah

--- routes/employees.js
+++ routes/employees.js
@@ -1,18 +1,22 @@
 var express = require('express')
 var router = express.Router()
+const employeesControllers = require ('../controllers/employees');
+const msql = require('/mysql')
+const pool = require('../mysql/connection')
 
+pool.query('SELECT * FROM employees', (err, rows) => {
+  if (err) {
+  console.log({ 'message': 'Error occurred: ' + err })
+  return res.status(500).send('Uh oh . . .')
+  }
+  res.json(rows)
+  });
 
-router.get('/employees', function (req, res) {
-  res.send("getting employees...")
-})
+router.get('/employees', employeesControllers.getEmployees)
 
-router.get('/employees/:id', function (req, res) {
-  res.send("getting employees..." )
-})
+router.get('/employees/:id', employeesControllers.getEmployeesById)
 
-router.get('/employees/firstname/:first_name', function (req, res) {
-  res.send("getting employees..." )
-})
+router.get('/employees/firstname/:first_name', employeesControllers.getEmployeesByFirstName)
 
 
 
