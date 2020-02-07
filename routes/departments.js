f0de7a7e7705726ce1bf763d55e85325b0dd5664
rodrieri000 <ericarodriguez7@outlook.com>
2020-02-06 19:38:49 -0600

    Fixed departments router err

--- routes/departments.js
+++ routes/departments.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const departmentsByIdController = require('../controllers/getDepartmentById')
 const departmentsByNameController = require('../controllers/getDepartmentByName')
 
-router.get('/:id', departmentsByIdController.getDepartmentById)
+router.get('/:dept_nogit ', departmentsByIdController.getDepartmentById)
 
-router.get('firstname/:first_name', departmentsByNameController.getDepartmentByName)
+router.get('deptName/:dept_name', departmentsByNameController.getDepartmentByName)
 
 module.exports = router;
