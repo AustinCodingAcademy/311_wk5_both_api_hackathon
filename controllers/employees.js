d15cd045149c691cee83cb92089e2648049cd3ad
Rob W <robpwatkins@gmail.com>
2020-02-08 19:28:13 -0600

    We almost done yo

--- controllers/employees.js
+++ controllers/employees.js
@@ -2,9 +2,8 @@ const pool = require('../mysql/connection');
 const mysql = require('mysql');
 
 const getEmployees = ((req, res) => {
-  pool.query("SELECT * FROM employees limit 50", (err, rows) => {
-    // if (err) return handleSQLError(res, err)
-    console.log(err);
+  pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
+    if (err) return handleSQLError(res, err)
     return res.json(rows);
   })
 })
@@ -30,8 +29,45 @@ const getEmployeesByFirstName = ((req, res) => {
   })
 })
 
+
+// Gets all employees (limit 50) by typing in a department, i.e. "Development" or "Finance"
+const getEmployeesByDepartment = ((req, res) => {
+  let sql = `
+  SELECT first_name, last_name, dept_name 
+  FROM employees 
+  JOIN dept_emp ON employees.emp_no = dept_emp.emp_no 
+  JOIN departments 
+  ON dept_emp.dept_no = departments.dept_no 
+  WHERE dept_name = ?
+  LIMIT 50`;
+  sql = mysql.format(sql, [req.params.department])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+})
+
+
+// Gets all the employees (limit 50 again) by typing in a job title, i.e. "Engineer" or "Staff"
+const getEmployeesByJobTitle = ((req, res) => {
+  let sql = `
+  SELECT * 
+  FROM employees 
+  JOIN titles 
+  ON employees.emp_no = titles.emp_no 
+  WHERE title = ? 
+  LIMIT 50`;
+  sql = mysql.format(sql, [req.params.title])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+})
+
 module.exports = {
   getEmployees,
   getEmployeesById,
-  getEmployeesByFirstName
+  getEmployeesByFirstName,
+  getEmployeesByDepartment,
+  getEmployeesByJobTitle
 }
--- routes/employees.js
+++ routes/employees.js
@@ -9,6 +9,11 @@ router.get('/employees/:id', employeesControllers.getEmployeesById)
 
 router.get('/employees/firstname/:first_name', employeesControllers.getEmployeesByFirstName)
 
+// The two new routes I created for the new functions . . .
+router.get('/employees/department/:department', employeesControllers.getEmployeesByDepartment)
+
+router.get('/employees/titles/:title', employeesControllers.getEmployeesByJobTitle)
+
 
 
 
