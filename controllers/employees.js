952cf93177cad445583b31d68a37244da8017ab6
Oscar Garcia <oscar.gch@gmail.com>
2020-02-09 17:49:04 -0600

    updated a limit on employees

--- controllers/employees.js
+++ controllers/employees.js
@@ -3,7 +3,7 @@ const pool = require('../mysql/connection')
 const { handleSQLError } = require('../mysql/error')
 
 const getEmployees = (req, res) => {
-  pool.query("SELECT * FROM employees.employees", (err, rows) => {
+  pool.query("SELECT * FROM employees.employees LIMIT 50;", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
     // return res.json('getting employees...');
