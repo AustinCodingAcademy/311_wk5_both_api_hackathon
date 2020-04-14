d2434a201f362c71eb88f48a4a02e514ccd5c851
crashdaddy <kennethscoggins@mail.com>
2020-04-13 21:26:54 -0500

    get all employees works

--- controllers/employees.js
+++ controllers/employees.js
@@ -3,14 +3,18 @@ const pool = require('../mysql/connection')
 const { handleSQLError } = require('../mysql/error')
 
 
-// show all the comments
+// show all the employees but limit the results to 50
 const getAllEmployees = (req, res) => {
+
+  // set the sql query
+  let sql = "SELECT * FROM employees LIMIT 50";
     
-      // SELECT ALL USERS
-  pool.query("SELECT * FROM employees", (err, rows) => {
+  // run the query
+  pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
   })
 };
 
+//export the functions we just set up
 module.exports = { getAllEmployees};
