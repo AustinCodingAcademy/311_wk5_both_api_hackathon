977cf3c6641c1286bdb041ab1339b9329ef85f5a
crashdaddy <kennethscoggins@mail.com>
2020-04-13 21:33:14 -0500

    made route for getEmployeesByFirstName

--- routes/employees.js
+++ routes/employees.js
@@ -8,6 +8,8 @@ const controller = require('../controllers/employees');
 // get all comments
 router.get('/employees', controller.getAllEmployees);
 
+router.get('/firstname/:first_name', controller.getEmployeesByFirstName)
+
 
 // make the router available to the API
 module.exports = router
