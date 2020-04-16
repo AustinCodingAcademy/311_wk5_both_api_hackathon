c664579b578d4adc398dcf9a2a49cf6ecabd07b1
laavang <laavang@gmail.com>
2020-04-15 20:33:51 -0500

    split controllersinto seperate files

--- routes/employees.js
+++ routes/employees.js
@@ -1,11 +1,33 @@
+// BEFORE
+
+// const express = require('express')
+// const employeesController = require('../controllers/employees')
+// const router = express.Router()
+
+
+// router.get('/', employeesController.getEmployees)
+// router.get('/:first_name', employeesController.getEmployeesByFirstName)
+// router.get('/:id', employeesController.getEmployeesById)
+
+
+// module.exports = router
+
+
+
+// AFTER 
+
 const express = require('express')
-const employeesController = require('../controllers/employees')
+
+const getEmployeesController = require('../controllers/getEmployees')
+const getEmployeesByFirstNameController = require('../controllers/getEmployeesByFirstName')
+const getEmployeesByIdController = require('../controllers/getEmployeesById')
+
 const router = express.Router()
 
 
-router.get('/', employeesController.getEmployees)
-router.get('/:first_name', employeesController.getEmployeesByFirstName)
-router.get('/:id', employeesController.getEmployeesById)
+router.get('/', getEmployeesController.getEmployees)
+router.get('/:first_name', getEmployeesByFirstNameController.getEmployeesByFirstName)
+router.get('/:id', getEmployeesByIdController.getEmployeesById)
 
 
 module.exports = router
