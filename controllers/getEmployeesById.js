dcf92a9471cb17e49e6966027dd8cbbc801844c2
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-06-23 22:18:46 -0500

    updated byId, firstName controllers, added errors.js

--- controllers/getEmployeesById.js
+++ controllers/getEmployeesById.js
@@ -1,5 +1,19 @@
+const mysql = require('mysql')
+const pool = require('../mysql/connection')
+const handleSQLError = require('../mysql/error')
+
 const getEmployeesById = (req, res) => {
-  res.send("getting employees")
+  let sql = 'SELECT * FROM ?? WHERE ?? = ?'
+  const replacements = ["employees", "emp_no", req.params.id]
+  sql = mysql.format(sql, replacements)
+
+  //alt sql syntax
+  //let sql = `SELECT * FROM employees WHERE emp_no = ${req.params.id}`
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows)
+  })
 }
 
 module.exports = {getEmployeesById}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const employeesRouter = require('./routes/employees')
 
 const app = express()
