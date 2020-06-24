338f4d90b2c5b8f398c7028ffce03e3c209d5271
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-06-23 20:23:37 -0500

    employees routes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+//const bodyParser = require('body-parser')
+const employeesRouter = require('./routers/employees')
+
 const app = express()
 const port = process.env.PORT || 4001
+
+app.use('/employees', employeesRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to our API')
 })
-
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}`)
