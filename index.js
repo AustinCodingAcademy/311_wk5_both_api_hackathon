6056b1385d469d1dcb07b6af7ccd9e9fb326e977
Sean Yager <contact@seanyager.com>
2020-06-23 20:11:44 -0500

    server is now working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("re-testing")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4001
+
+app.get('/', (req, res) => res.send('WELCOME TO OUR API'))
+
+app.listen(port, () => {
+    console.log('   app is listening on:', port)
+})
