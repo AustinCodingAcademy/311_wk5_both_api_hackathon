ad2e2cfba077d9bbdfe1f9297bea8648f94eed98
evantwidwell <evanski@gmail.com>
2020-06-23 21:13:38 -0500

    mysql instegration

---
+++ mysql/connection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log('creating connection...')
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: '34.70.106.43',
+        user: 'root',
+        password: 'Thrillh0!',
+        database: 'employees',
+      })
+
+      return this.pool
+    }
+
+    return this.pool
+  }
+}
+
+const instance = new Connection()
+
+module.exports = instance;
