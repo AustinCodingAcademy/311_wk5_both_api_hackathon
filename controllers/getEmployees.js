dc2a5d60b8a66a4ba75675d87ddbc6b2b8efa748
Ben Edwards <benedwrds@yahoo.com>
2020-06-24 18:31:45 -0500

    getemployess

--- controllers/getEmployees.js
+++ controllers/getEmployees.js
@@ -1,5 +1,16 @@
+const mysql = require('mysql')
+const pool = require('../mysql/connection')
+const handleSQLError = require('../mysql/errors')
+
 const getEmployees = (req, res) => {
- res.send("getting employees") 
+ let sql = "select * from ?? LIMIT 50;"
+ const replacements = ['employees']
+  sql = mysql.format(sql, replacements)
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows)
+  })
 }
 
 module.exports = { getEmployees }
