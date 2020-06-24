8f6c4091550eb8779ec11b602c89e7836c5ff70d
Sean Yager <contact@seanyager.com>
2020-06-24 13:17:05 -0500

    organized require()

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,6 @@
 const express = require('express')
+const controller = require('../controllers/getEmployees.js')
 const router = express.Router()
-const controller = require('../controllers/getEmployees')
 
 router.get('/', controller.getEmployees)
 router.get('/:emp_no', controller.getEmployeesById)
