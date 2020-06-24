6edd45ddc4ed6cb19824b1eef5f133cea65bc901
Sean Yager <contact@seanyager.com>
2020-06-23 20:25:27 -0500

    routers created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4001
+const employees = require('./orutes/employees')
 
 app.get('/', (req, res) => res.send('WELCOME TO OUR API'))
 
