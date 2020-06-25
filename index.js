12ce191b734077a74409c44175b0f7d258959fa8
evantwidwell <evanski@gmail.com>
2020-06-25 18:01:28 -0500

    basic express set up

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('re-testing')
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 4001;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our server!')
+})
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}!`);
+ });
