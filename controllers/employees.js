8d0b0e370ef61bdb6b631fa09034224893f04eb1
peter gilkey <peter_gilkey@yahoo.com>
2020-06-25 23:55:45 -0500

    added firstname route, required mysql in controller

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,15 +1,19 @@
+const mysql = require('mysql');
 const pool = require("../mysql/connections")
 
 
 const getEmployees = (req, res) => {
     pool.query('SELECT * FROM employees LIMIT 1000', (err, rows) => {
+      if (err) {
+        res.status(500).send("An error occured");
+      }
       return res.json(rows);
   })
 }
 
 let getEmployeeByID = (req, res) => {
     let id = req.params.id
-    let sql = 'SELECT * FROM employees WHERE id = ?'
+    let sql = 'SELECT * FROM employees WHERE emp_no = ?'
     sql = mysql.format(sql, [id])
 
     pool.query(sql, (err, rows) => {
@@ -17,4 +21,14 @@ let getEmployeeByID = (req, res) => {
     })
 }
 
-module.exports = { getEmployees, getEmployeeByID }
+const getEmployeeByFirstName = (req, res) => {
+    let name = req.params.first_name;
+    let sql = 'SELECT * FROM employees WHERE first_name = ? LIMIT 1000'
+    sql = mysql.format(sql, [name]);
+
+    pool.query(sql, (err, rows) => {
+        return res.json(rows);
+    });
+}
+
+module.exports = { getEmployees, getEmployeeByID, getEmployeeByFirstName }
--- routes/employees.js
+++ routes/employees.js
@@ -6,10 +6,7 @@ router.get('/', controller.getEmployees);
 
 router.get('/:id', controller.getEmployeeByID);
 
-router.get('/firstname/:first_name', (req, res) => {
-  let name = req.params.first_name;
-  res.send(`Getting employees by first name: ${name}`);
-});
+router.get('/firstname/:first_name', controller.getEmployeeByFirstName);
 
 
 module.exports = router;
