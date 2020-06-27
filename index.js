4e1e092aac4485aa825d7f4336151488a07c7531
Ben Edwards <benedwrds@yahoo.com>
2020-06-27 15:43:09 -0500

    empty array

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const employeesRouter = require('./routes/employees')
+const salariesRouter = require('./routes/salaries')
 
 const app = express()
 const port = process.env.PORT || 4001
 
 app.use('/employees', employeesRouter)
+
+app.use('/salaries', salariesRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to our API')
