64d97aa5a92b4f08e0a8b33346592a6edf4911c8
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-08-31 18:34:42 -0500

    fixed server

--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const employeesController = require('../controllers/employees')
 
-router.get('/', employeesController.getAllEmployees)
+router.get('/', employeesController.getEmployees)
 
 router.get('/:id', employeesController.getEmployeeById)
 
-router.get('firstname/:first_name', employeesController.getEmployeeByFirstName)
+// router.get('firstname/:first_name', employeesController.getEmployeeByFirstName)
+
 module.exports = router
