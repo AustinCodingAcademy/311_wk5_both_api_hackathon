b6928d0a7ac77c94cd461527239bb7075e7db417
TEldridge <skippydrumskins@yahoo.com>
2020-08-31 17:38:48 -0500

    completed employee routes and dept routes

--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const employeesController = require('../controllers/employees')
 
 router.get('/', employeesController.getAllEmployees)
 
 router.get('/:id', employeesController.getEmployeeById)
 
 router.get('firstname/:first_name', employeesController.getEmployeeByFirstName)
+
+router.post('/', employeesController.createEmployee)
+
+router.put('/:id', employeesController.updateEmployee)
+
+router.delete('firstname/:first_name', employeesController.deleteEmployee)
+
 module.exports = router
