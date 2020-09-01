c06ab7bb72c0ae3a08a44f98334fe9859808d058
TEldridge <skippydrumskins@yahoo.com>
2020-09-01 10:47:20 -0500

    fixed department path bug

--- routes/departments.js
+++ routes/departments.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const departmentsController = require('../controllers/departments')
+const departmentsController = require('../controllers/departments/create.js')
+const departmentsController = require('../controllers/departments/delete.js')
+const departmentsController = require('../controllers/departments/getall.js')
+const departmentsController = require('../controllers/departments/getbyid.js')
+const departmentsController = require('../controllers/departments/put.js')
 
 router.get('/', departmentsController.getDepartments)
 
