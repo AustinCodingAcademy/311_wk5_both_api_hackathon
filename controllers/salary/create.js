f147a88a2290481800a3e5a12f5195dbd0763e38
Kevin Valias <Kevbodyworks@gmail.com>
2020-09-01 16:32:54 -0500

    added-create-salary

---
+++ controllers/salary/create.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+
+const createSalary = (req, res) => {
+  // INSERT INTO USERS FIRST AND LAST NAME 
+  let sql = "INSERT INTO salaries (emp_no, salary, from_date, to_date)VALUES(?, ?, ?, ?);"
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [req.params.emp_no, req.body.salary, req.body.from_date, req.body.to_date])
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ newId: results.insertId });
+  })
+}
+
+
+
+
+
+
+module.exports = {
+     createSalary
+}
