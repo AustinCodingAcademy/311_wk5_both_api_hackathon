7972406dc7e51c442cc4458aa3a594a7a00a337e
TEldridge <skippydrumskins@yahoo.com>
2020-09-01 12:14:25 -0500

    fixed employee route paths

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const employeesController = require('../controllers/employees')
+const createEmployeeController = require('../controllers/employees/create')
+const updateEmployeeController = require('../controllers/employees/update')
+const deleteEmployeeController = require('../controllers/employees/delete')
 
 router.get('/', employeesController.getEmployees)
 
@@ -8,10 +11,10 @@ router.get('/:id', employeesController.getEmployeeById)
 
 router.get('/firstname/:first_name', employeesController.getEmployeeByFirstName)
 
-// router.post('/', employeesController.createEmployee)
+router.post('/', createEmployeeController.createEmployee)
 
-// router.put('/:id', employeesController.updateEmployee)
+router.put('/:id', updateEmployeeController.updateEmployee)
 
-// router.delete('firstname/:first_name', employeesController.deleteEmployee)
+router.delete('firstname/:first_name', deleteEmployeeController.deleteEmployee)
 
 module.exports = router
