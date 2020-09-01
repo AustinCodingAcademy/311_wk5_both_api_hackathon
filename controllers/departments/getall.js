5c90dd1f41f900d1d26063d566300514687d9a86
DavidStephenss <dvdstephensd@gmail.com>
2020-09-01 10:31:25 -0500

    added folder organization

---
+++ controllers/departments/getall.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+
+const getDepartments = (req, res) => {
+    let sql = ` SELECT *
+                FROM departments`
+    sql = mysql.format(sql, [])
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
+}
+
+module.exports = {
+  getDepartments 
+}
---
+++ controllers/departments/getbyid.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+const getDepartmentsById = (req, res) => {
+  // SELECT USERS WHERE ID = <REQ PARAMS ID>
+  let sql = "SELECT * FROM departments WHERE id = ?"
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [req.params.id])
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
+module.exports = {
+  getDepartmentsById 
+}
