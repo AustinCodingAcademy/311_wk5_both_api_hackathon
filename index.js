9c57acd8a7c7154a35a61aa7d4992d5c2d135fc0
Lance Hammond Jr <lance.hammondjr@gmail.com>
2020-09-02 10:25:31 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let i = 0;
+//index.js
+const express = require("express");
+const usersRouter = require('./routers/routerUsers');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 4001;
+
+app.use('/users', usersRouter)
+
+app.get('/', (req, res) => {
+res.send('Welcome to our server!')
+})
+
+app.listen(port, () => { console.log(Listening on port ${port}!); });
---
+++ sql/error.js
@@ -0,0 +1,7 @@
+//sql/error.js
+const handleSQLError = (res, err) => {
+    console.log('SQL Error: ', err)
+    return res.status(500).send('An unexpected error occurred');
+    }
+    
+    module.exports = { handleSQLError }
