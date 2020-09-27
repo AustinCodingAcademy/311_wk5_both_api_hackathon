b958bbef833d9986f25073aaa1da98c002a068e4
Shelby-Jean <shelby.gottschall@gmail.com>
2020-09-27 18:31:49 -0400

    added controller

---
+++ controllers/employeesController.js
@@ -0,0 +1,16 @@
+
+
+const getEmployees = (req, res) => {
+  return res.send('getting employees...');
+}
+
+const getEmployeesById = (req, res) => {
+  return res.send('getting employees...');
+}
+
+const getEmployeesByFirstName = (req, res) => {
+  return res.send('getting employees...');
+}
+
+
+module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
