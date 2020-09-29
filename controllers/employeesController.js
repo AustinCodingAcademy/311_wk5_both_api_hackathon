4edfaf7a8d8ae681960d4a823a1487903ecef2f6
capatterson1970 <capatterson70@yahoo.com>
2020-09-28 20:27:59 -0500

    Finished Product and tested

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -3,6 +3,7 @@ const pool = require('../mysql/connection');
 const { handleSQLError } = require('../mysql/error');
 
 const getEmployees = (req, res) => {
+  console.log('Test1')
   pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
@@ -10,6 +11,7 @@ const getEmployees = (req, res) => {
 }
 
 const getEmployeesById = (req, res) => {
+  console.log('Test2')
   let sql = "SELECT * FROM employees WHERE emp_no = ?";
   sql = mysql.format(sql, [req.params.id])
   pool.query(sql, (err, rows) => {
@@ -20,6 +22,7 @@ const getEmployeesById = (req, res) => {
 }
 
 const getEmployeesByFirstName = (req, res) => {
+  console.log('Test3')
   let sql = "SELECT * FROM employees WHERE first_name = ? LIMIT 50";
   sql = mysql.format(sql, [req.params.first_name])
   pool.query(sql, (err, rows) => {
@@ -29,5 +32,33 @@ const getEmployeesByFirstName = (req, res) => {
   })
 }
 
+const getEmployeesSalary = (req, res) => {
+  console.log('Salary');
+  pool.query("SELECT * FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no LIMIT 50", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
 
-module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
+const getEmployeesByIdSalary = (req, res) => {
+  let sql = "SELECT * FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no WHERE employees.emp_no = ?";
+  sql = mysql.format(sql, [req.params.id])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    console.log(rows)
+    return res.json(rows);
+  })
+}
+
+const getEmployeesByFirstNameSalary = (req, res) => {
+  let sql = "SELECT * FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no WHERE first_name = ? LIMIT 50";
+  sql = mysql.format(sql, [req.params.first_name])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    console.log(req.params.first_name)
+    return res.json(rows);
+  })
+}
+
+
+module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName, getEmployeesSalary, getEmployeesByIdSalary, getEmployeesByFirstNameSalary }
--- routes/employeesRouter.js
+++ routes/employeesRouter.js
@@ -7,8 +7,14 @@ const jsonParser = bodyParser.json();
 
 router.get('/', employeesController.getEmployees);
 
-router.get('/:id', employeesController.getEmployeesById);
+router.get('/byid/:id', employeesController.getEmployeesById);
 
 router.get('/byFirstName/:first_name', employeesController.getEmployeesByFirstName);
+
+router.get('/salary', employeesController.getEmployeesSalary);
+
+router.get('/byid/salary/:id', employeesController.getEmployeesByIdSalary);
+
+router.get('/byFirstName/salary/:first_name', employeesController.getEmployeesByFirstNameSalary);
 
 module.exports = router;;
