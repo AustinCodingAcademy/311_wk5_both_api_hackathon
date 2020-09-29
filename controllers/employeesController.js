b45814fa663d66ae49fe00145e8211737bf93139
capatterson1970 <capatterson70@yahoo.com>
2020-09-28 19:42:02 -0500

    Added SQL and Error

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,15 +1,32 @@
-
+const mysql = require('mysql');
+const pool = require('../mysql/connection');
+const { handleSQLError } = require('../mysql/error');
 
 const getEmployees = (req, res) => {
-  return res.send('getting employees...');
+  pool.query("SELECT * FROM employees LIMIT 50", (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
 }
 
 const getEmployeesById = (req, res) => {
-  return res.send('getting employees...');
+  let sql = "SELECT * FROM employees WHERE emp_no = ?";
+  sql = mysql.format(sql, [req.params.id])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    console.log(rows)
+    return res.json(rows);
+  })
 }
 
 const getEmployeesByFirstName = (req, res) => {
-  return res.send('getting employees...');
+  let sql = "SELECT * FROM employees WHERE first_name = ? LIMIT 50";
+  sql = mysql.format(sql, [req.params.first_name])
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    console.log(req.params.first_name)
+    return res.json(rows);
+  })
 }
 
 
