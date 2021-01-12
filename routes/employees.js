7633d6d04d5a0c0156673d56ffdab51a829af19e
@willmcclure <willbmcclure@gmail.com>
2021-01-12 14:08:23 -0600

    partially completed controllers and updated route file

--- routes/employees.js
+++ routes/employees.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const employeesController = require('../controllers/employees')
 const router = express.Router()
+
 // const employees = WAITING TO HOOK MY MYSQL
 
-app.get("/", (req, res) => {
-    res.send('getting employees...')
-})
+// app.get("/", (req, res) => {
+//     res.send('getting employees...')
+// })
 
-app.get("/:id", (req, res) => {
-    res.send('getting employees...')
-})
+router.get('/', employeesController.getEmployees);
 
-app.get("firstname/:first_name", (req, res) => {
-    res.send('getting employees...')
-})
+// app.get("/:id", (req, res) => {
+//     res.send('getting employees...')
+// })
+
+router.get('/', employeesController.getEmployeesById);
+
+
+// app.get("firstname/:first_name", (req, res) => {
+//     res.send('getting employees...')
+// })
+
+router.get('/', employeesController.getEmployeesByFirstName);
+
 
 module.exports = router
