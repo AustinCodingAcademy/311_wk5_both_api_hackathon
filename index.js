93723d37b7238e31ff8b0ecf5a9b77ed53089d68
Matt Carter <carter.matthew@me.com>
2021-01-12 20:32:19 -0500

    express server configured

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Test - Testing - restart")
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json('Welcome to our API!')
+})
+
+app.listen('4001', () => console.log('Server started on port 4001'))
