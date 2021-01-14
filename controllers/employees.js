7233caa702380843f413093a3eca183b183732b1
Matt Carter <carter.matthew@me.com>
2021-01-13 21:26:45 -0500

    employees by id

--- controllers/employees.js
+++ controllers/employees.js
@@ -11,7 +11,19 @@ const getEmployees = (req, res) => {
   })
 }
 
-const getEmployeesById;
+const getEmployeesById = (req, res) => {
+    let id = req.params.id
+
+    let sql = "SELECT FROM employees WHERE id = ?"
+    let value = id
+    
+    sql = mysql.format(sql, value)
+
+    pool.query(sql, (err, results) => {
+        if (err) throw err
+        return res.json(results)
+    })
+}
 
 const getEmployeesByFirstName;
 
