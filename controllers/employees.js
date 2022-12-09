f98eee4f99949c7b1e75ed7e47de3ff3fb1caf58
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-12-08 22:29:44 -0600

    working through some steps

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,21 +1,37 @@
 const mysql = require('mysql')
-const pool = require('../sql/connection')
-const { handleSQLError } = require('../sql/error')
+const pool = require('../mysql/connections')
+const sqlErrorHandler = require("../mysql/error")
 
 const getEmployees = (req, res) => {
-
+    pool.query('SELECT * FROM employees LIMIT 50', (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
 }
 
 const getEmployeesById = (req, res) => {
+    let sql = 'SELECT * FROM employees WHERE emp_no = ?'
+    sql = mysql.format(sql, [req.params.id])
 
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
 }
 
 const getEmployeesByFirstName = (req, res) => {
+    let sql = 'SELECT * FROM employees WHERE first_name = ?'
+    sql = mysql.format(sql, [req.params.first_name])
 
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
 }
 
 
 module.exports = {
     getEmployees,
     getEmployeesById,
-    getEmployeesByFirstName }
+    getEmployeesByFirstName
+}
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-const employees = require('./routes/employees')
+const employees = require('./routes/employees');
 
 const port = process.env.PORT || 4001;
 
 const bodyParser = require('body-parser')
-const employers = require('./routers/employers')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(employers)
+
+app.use('/employees', employees)
 
 
 
