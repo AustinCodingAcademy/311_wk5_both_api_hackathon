0647b5658df75baccd3845ce72747af5c4e18710
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-12-16 10:26:40 -0600

    saved salary controller file

--- controllers/salaries.js
+++ controllers/salaries.js
@@ -3,32 +3,41 @@ const pool = require('../mysql/connections')
 const handleSQLError = require("../mysql/error")
 
 const getSalary = (req, res) => {
-    pool.query('SELECT * FROM employees LIMIT 50', (err, rows) => {
-        if (err) return handleSQLError(res, err)
-        return res.json(rows);
-    })
+
+    // Logic to access data here, similar to 'employees'
+
+    // pool.query('SELECT * FROM employees LIMIT 50', (err, rows) => {
+    //     if (err) return handleSQLError(res, err)
+    //     return res.json(rows);
+    // })
 }
 
 const getSalaryById = (req, res) => {
-    let sql = 'SELECT * FROM ?? WHERE ?? = ?'
-    const replacements = ["employees", "emp_no", req.params.emp_no]
-    sql = mysql.format(sql, replacements)
 
-    pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
-        return res.json(rows);
-    })
+    // Logic to access data here, similar to 'employees'
+
+    // let sql = 'SELECT * FROM ?? WHERE ?? = ?'
+    // const replacements = ["employees", "emp_no", req.params.emp_no]
+    // sql = mysql.format(sql, replacements)
+
+    // pool.query(sql, (err, rows) => {
+    //     if (err) return handleSQLError(res, err)
+    //     return res.json(rows);
+    // })
 }
 
 const getSalaryByFirstName = (req, res) => {
-    let sql = 'SELECT * FROM ?? WHERE ?? = ?'
-    const replacements = ["employees", "first_name", req.params.first_name]
-    sql = mysql.format(sql, replacements)
 
-    pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
-        return res.json(rows);
-    })
+    // Logic to access data here, similar to 'employees'
+
+    // let sql = 'SELECT * FROM ?? WHERE ?? = ?'
+    // const replacements = ["employees", "first_name", req.params.first_name]
+    // sql = mysql.format(sql, replacements)
+
+    // pool.query(sql, (err, rows) => {
+    //     if (err) return handleSQLError(res, err)
+    //     return res.json(rows);
+    // })
 }
 
 module.exports = {
