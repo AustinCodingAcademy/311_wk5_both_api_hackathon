404acb9ab0be931550788f9acf80509b035ab683
Caroline-Patrick <carolinespatrick@gmail.com>
2022-12-26 18:17:05 -0600

    getSalary and get SalaryById functions tested and working

--- controllers/salaries.js
+++ controllers/salaries.js
@@ -6,24 +6,24 @@ const getSalary = (req, res) => {
 
     // Logic to access data here, similar to 'employees'
 
-    // pool.query('SELECT * FROM employees LIMIT 50', (err, rows) => {
-    //     if (err) return handleSQLError(res, err)
-    //     return res.json(rows);
-    // })
+    pool.query('SELECT * FROM salaries LIMIT 50', (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
 }
 
 const getSalaryById = (req, res) => {
 
     // Logic to access data here, similar to 'employees'
 
-    // let sql = 'SELECT * FROM ?? WHERE ?? = ?'
-    // const replacements = ["employees", "emp_no", req.params.emp_no]
-    // sql = mysql.format(sql, replacements)
+    let sql = 'SELECT * FROM ?? WHERE ?? = ?'
+    const replacements = ["salaries", "emp_no", req.params.emp_no]
+    sql = mysql.format(sql, replacements)
 
-    // pool.query(sql, (err, rows) => {
-    //     if (err) return handleSQLError(res, err)
-    //     return res.json(rows);
-    // })
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows);
+    })
 }
 
 const getSalaryByFirstName = (req, res) => {
